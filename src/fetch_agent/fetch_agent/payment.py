0c6858412212a938010ec4c8cc58e38262e6952c
leToxic <sebastian.plasek@htlstp.at>
2024-12-09 13:58:15 +0000

    lint????

--- src/fetch_agent/fetch_agent/payment.py
+++ src/fetch_agent/fetch_agent/payment.py
@@ -1,4 +1,4 @@
-﻿from uagents import Context
+from uagents import Context
 from aca_protocols.ac_payment_protocol import PaymentRequest, TransactionInfo, DENOM
 from aca_protocols.ac_charging_protocol import CarFinishedChargingInfo
 from aca_protocols.property_query_protocol import (
